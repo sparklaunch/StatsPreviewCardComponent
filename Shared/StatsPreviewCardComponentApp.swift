602bfe249831bd6b2691aaae73f5e8b248fa4a53
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-25 09:13:49 +0900

    Initial Commit

---
+++ Shared/StatsPreviewCardComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsPreviewCardComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/25.
+//
+
+import SwiftUI
+
+@main
+struct StatsPreviewCardComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
